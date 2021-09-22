88b163d037f2ed5162e9e06e098b95a7869efc5e
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-09-22 08:20:34 +0000

    iterator pattern

---
+++ iterator/BookShelf.java
@@ -0,0 +1,24 @@
+package iterator;
+
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+    private ArrayList<Book> books = new ArrayList<>();
+
+    public Book getBookAt(int index) {
+        return books.get(index);
+    }
+    
+    public void appendBook(Book book) {
+        this.books.add(book);
+    }
+
+    public int getLength() {
+        return books.size();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
---
+++ iterator/BookShelfIterator.java
@@ -0,0 +1,28 @@
+package iterator;
+
+public class BookShelfIterator implements Iterator {
+    private BookShelf bookShelf;
+    private int index;
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < this.bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object next() {
+        Book book = this.bookShelf.getBookAt(index);
+        index++;
+        return book;
+    }
+    
+}
