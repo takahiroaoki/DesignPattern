c81b49f27ff6bd4c392c579ed4fd718f8deca03b
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-09-24 11:49:46 +0000

    singleton

---
+++ singleton/Tripleton.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class Tripleton {
+    private static Tripleton[] tripletons = {
+        new Tripleton(0),
+        new Tripleton(1),
+        new Tripleton(2),
+    };
+    private int id;
+    private Tripleton(int id) {
+        System.out.println("id = " + id + " is created.");
+        this.id = id;
+    }
+    public static Tripleton getInstance(int id) {
+        return tripletons[id];
+    }
+    public String ofString() {
+        return "Tripleton of id = " + this.id;
+    }
+}
