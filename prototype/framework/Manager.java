524028f020defbc5900c270469761cd075a054c0
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-09-25 12:31:41 +0000

    prototype pattern

---
+++ prototype/framework/Manager.java
@@ -0,0 +1,15 @@
+package prototype.framework;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Manager {
+    private Map<String, Product> showCase = new HashMap<>();
+    public void register(String name, Product proto) {
+        showCase.put(name, proto);
+    }
+    public Product create(String protoName) {
+        Product p = showCase.get(protoName);
+        return p.createClone();
+    }
+}
