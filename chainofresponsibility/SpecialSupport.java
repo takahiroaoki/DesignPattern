38328fe2d60d8cc14211fe0e9f5f0a49c7710cc1
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-10-06 05:54:44 +0000

    chain of responsibility pattern

---
+++ chainofresponsibility/SpecialSupport.java
@@ -0,0 +1,19 @@
+package chainofresponsibility;
+
+public class SpecialSupport extends Support {
+    private int number;// この番号だけ解決できる
+
+    public SpecialSupport(String name, int number) {
+        super(name);
+        this.number = number;
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        if (trouble.getNumber() == number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
