96743b1ad50e6ecdd07e36b63e37ebd8c39d608f
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-10-18 09:30:40 +0000

    interpreter pattern

---
+++ interpreter/ExecuteInterpreter.java
@@ -0,0 +1,22 @@
+package interpreter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ExecuteInterpreter {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("interpreter/program.txt"));
+            String text;
+            while ((text = reader.readLine()) != null) {
+                System.out.println("text = \"" + text + "\"");
+                Node node = new ProgramNode();
+                node.parse(new Context(text));
+                System.out.println("node = " + node.toString());
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ interpreter/Node.java
@@ -0,0 +1,6 @@
+package interpreter;
+
+public abstract class Node {
+    public abstract void parse(Context context) throws ParseException;
+    public abstract String toString();
+}
